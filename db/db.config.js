c87fff4e509b6729633b36f10ec4f52e21e745da
Jaime Cabrera <jycabrerap@unl.edu.ec>
2021-09-09 21:35:35 -0500

    add postgress

--- db/db.config.js
+++ db/db.config.js
@@ -1,7 +1,15 @@
+// module.exports = {
+//   HOST: "localhost",
+//   USER: "root",
+//   PASSWORD: "",
+//   DB: "prog2",
+//   dialect: "mysql",
+// };
+
 module.exports = {
-  HOST: "localhost",
-  USER: "root",
-  PASSWORD: "",
-  DB: "prog2",
-  dialect: "mysql",
+  HOST: "ec2-44-198-80-194.compute-1.amazonaws.com",
+  USER: "chjnxgdpiidxwz",
+  PASSWORD: "b4ed9a6219706ec5309f0bd9f4daf42de8aca771a4aa2ca420c5490212a2440c",
+  DB: "d4jgfbkeqolqac",
+  diaclect: "postgres",
 };
